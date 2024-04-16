e9d970083ee3afa5ce3f22ae5850aed4c800a9f7
Tuancs <tuancs@tuandata.local>
2024-04-17 02:32:29 +0700

    Add pipeline for integrating data from ftp source to ftp target

---
+++ dags/configs/ftps.py
@@ -0,0 +1,9 @@
+class FTPSource:
+    HOST = 'ftp-server'
+    USER = 'source'
+    PASSWD = 'source'
+
+class FTPTarget:
+    HOST = 'ftp-server'
+    USER = 'target'
+    PASSWD = 'target'
--- dags/sftp_file_transfer_dag.py
+++ dags/sftp_file_transfer_dag.py
@@ -1,33 +1,32 @@
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
+from configs.ftps import FTPSource, FTPTarget
+from libs.ftp import integrate_ftp_server
+from airflow.operators.python import PythonOperator
 
-# Define the default arguments for the DAG
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024, 4, 11),
-    'retries': 1,
+    'depends_on_past': False,
+    'start_date': datetime(2024, 4, 14),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
 }
 
-# Define the DAG
 dag = DAG(
-    'hello_world_dag',
+    'sftp_transfer_dag',
     default_args=default_args,
-    description='A simple DAG that prints Hello, World!',
-    schedule_interval=None,  # Set to None to disable automatic scheduling
+    description='Transfer files from source SFTP server to target SFTP server',
+    schedule_interval=None,
 )
 
-# Define the Python function to print "Hello, World!"
-def print_hello():
-    print("Hello, World!")
-
-# Define the task using the PythonOperator
-print_hello_task = PythonOperator(
-    task_id='print_hello_task',
-    python_callable=print_hello,
-    dag=dag,
+integrate_ftp_server_files = PythonOperator(
+    python_callable=integrate_ftp_server,
+    op_kwargs={
+        'source': FTPSource,
+        'target': FTPTarget,
+    },
+    task_id='integrate_ftp_server_files',
+    dag=dag
 )
 
-# Set task dependencies
-print_hello_task
-
+integrate_ftp_server_files
